c951149c73810f7da450125df7e7406c962c4ce2
SdangondLT <sdangond@lean-tech.io>
2022-03-15 14:58:43 -0500

    added a reverse matrix

--- Program.cs
+++ Program.cs
@@ -10,15 +10,37 @@ namespace matrix
             //Console.WriteLine("Ingrese el tamaño de las columnas: ");
 
             //AL DERECHO
+            //int[,] matriz = new int[9, 9];
+            //int counter = 0;
+
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        matriz[i, j] = counter;
+            //        counter = counter + 1;
+            //    }
+            //}
+
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        Console.Write(" [ " + matriz[i, j] + " ] ");
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //AL REVES PERO NORMAL
             int[,] matriz = new int[9, 9];
-            int counter = 0;
+            int counter = 81;
 
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < matriz.GetLength(1); j++)
                 {
                     matriz[i, j] = counter;
-                    counter = counter + 1;
+                    counter = counter - 1;
                 }
             }
 
