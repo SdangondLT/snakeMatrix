a067b42b721ce5fcb262f37874b1d7f256914efb
SdangondLT <sdangond@lean-tech.io>
2022-03-15 18:16:12 -0500

    added a matrix like a snake

--- Program.cs
+++ Program.cs
@@ -32,23 +32,59 @@ namespace matrix
             //Console.ReadKey();
 
             //AL REVES PERO NORMAL
+            //int[,] matriz = new int[9, 9];
+            //int counter = 81;
+
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        matriz[i, j] = counter;
+            //        counter = counter - 1;
+            //    }
+            //}
+
+            //for (int i = 0; i < matriz.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < matriz.GetLength(1); j++)
+            //    {
+            //        Console.Write(" [ " + matriz[i, j] + " ] ");
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //AL REVES COMO CULEBRITA
             int[,] matriz = new int[9, 9];
             int counter = 81;
 
-            for (int i = 0; i < matriz.GetLength(0); i++)
+            for (int fila = 0; fila < matriz.GetLength(0); fila++)
             {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    matriz[i, j] = counter;
-                    counter = counter - 1;
+                if (fila % 2 == 0)
+                {//si es una fila par debe hacer el calculo normal de llenar la matriz
+
+                    for (int colum = 0; colum < matriz.GetLength(1); colum++)
+                    {
+                        matriz[fila, colum] = counter;
+                        counter = counter - 1;
+                    }
+                }
+                else
+                {//si es una fila impar que empiece en la cola de su indice
+
+                    for (int colum = matriz.GetLength(1) - 1; colum >= 0; colum--)
+                    {
+                        matriz[fila, colum] = counter;
+                        counter = counter - 1;
+                    }
                 }
             }
 
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
-                for (int j = 0; j < matriz.GetLength(1); j++)
+                Console.WriteLine();
+                for (int colum = 0; colum < matriz.GetLength(1); colum++)
                 {
-                    Console.Write(" [ " + matriz[i, j] + " ] ");
+                    Console.Write(" [ " + matriz[i, colum] + " ] ");
                 }
             }
             Console.ReadKey();
